6cdad1bce5ead03a828d21194f8e38c2e3640b96
Abigail Rolling and Daren Schaad <arolling@gmail.com>
2016-05-10 11:45:02 -0700

    Set up initial file structure and API constants

---
+++ app/src/main/java/com/epicodus/faultyfootballleague/Constants.java
@@ -0,0 +1,23 @@
+package com.epicodus.faultyfootballleague;
+
+/**
+ * Created by Guest on 5/10/16.
+ */
+public class Constants {
+    public final String NFL_ARREST_BASE_URL = "http://nflarrest.com/api/v1/";
+
+    public final String NFL_CRIME_PATH_SEGMENT = "crime";
+    public final String NFL_TOP_PLAYERS_PATH_SEGMENT = "topPlayers";
+    public final String NFL_TOP_TEAMS_PATH_SEGMENT = "topTeams";
+    public final String NFL_TOP_CRIMES_PATH_SEGMENT = "topCrimes";
+    public final String NFL_TIMELINE_PATH_SEGMENT = "timeline";
+    public final String NFL_ARREST_DETAILS_PATH_SEGMENT = "arrests";
+    public final String NFL_TEAM_PATH_SEGMENT = "team";
+    public final String NFL_SEARCH_PATH_SEGMENT = "search";
+    public final String NFL_PLAYER_PATH_SEGMENT = "player";
+
+    public final String START_DATE_QUERY = "start_date";
+    public final String END_DATE_QUERY = "end_date";
+    public final String LIMIT_QUERY = "limit";
+
+}
---
+++ app/src/main/java/com/epicodus/faultyfootballleague/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.faultyfootballleague.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.faultyfootballleague.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
