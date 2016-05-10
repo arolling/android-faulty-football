a2f3eee1449799a396676b0b90c595051186f23e
Abigail Rolling and Daren Schaad <darenschaad@gmail.com>
2016-05-10 14:00:46 -0700

    Add font and colors

--- app/src/main/java/com/epicodus/faultyfootballleague/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/faultyfootballleague/ui/MainActivity.java
@@ -1,15 +1,26 @@
 package com.epicodus.faultyfootballleague.ui;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epicodus.faultyfootballleague.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.headerTextView) TextView mHeaderTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "Sports Jersey.ttf");
+        mHeaderTextView.setTypeface(face);
     }
 }
